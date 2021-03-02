3fd90088e87fcb44189e7f51a4b25e7f7f72bf1f
OlegKanev <79939693+OlegKanev@users.noreply.github.com>
2021-03-02 23:56:38 +0300

    Update WanderAI.cs

--- WanderAI.cs
+++ WanderAI.cs
@@ -20,37 +20,13 @@ public class WanderAI : MonoBehaviour
     public string randomPointName;
 
     private bool isNotWalking = true;
+    
     private void Start()
     {
         playerAnim = GetComponent<Animator>();
         StartCoroutine(State());
         audioSource = GetComponent<AudioSource>();
     }
-
-
-    //void Update()
-    //{
-    //    //if (Input.GetMouseButton(0))
-    //    //{
-    //    //    Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-    //    //    RaycastHit hit;
-
-    //    //    if (Physics.Raycast(ray, out hit))
-    //    //    {
-    //    //        agent.SetDestination(hit.point);
-    //    //        playerAnim.SetBool("isWalking", true);
-    //    //        playerAnim.SetBool("isRoaring", false);
-
-    //    //    }
-
-    //    //}
-
-    //    if (transform.position == agent.pathEndPosition)
-    //    {
-    //        playerAnim.SetBool("isWalking", false);
-
-    //    }
-    //}
 
     IEnumerator State()
     {
@@ -65,16 +41,7 @@ public class WanderAI : MonoBehaviour
             }
             yield return new WaitForSeconds(1);
         }
-        
     }
-
-    //private void State()
-    //{
-    //    currentState = Random.Range(1, 5);
-    //    SetState(currentState);
-    //    print("beginCor");
-    //    print(currentState);
-    //}
 
     private void Roar()
     {
@@ -117,7 +84,6 @@ public class WanderAI : MonoBehaviour
                 playerAnim.SetBool("isEating", true);
                 break;
         }
-
     }
 
     private void OnTriggerEnter(Collider other)
@@ -127,12 +93,6 @@ public class WanderAI : MonoBehaviour
             print("Destination");
             isNotWalking = true;
             playerAnim.SetBool("isWalking", false);
-            //StartCoroutine(State());
-            //currentState = Random.Range(1, 5);
-            //SetState(currentState);
-            //print(currentState);
         }
-        
     }
-    
 } 
